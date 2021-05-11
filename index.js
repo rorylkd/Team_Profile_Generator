e0a20fab4e49953e082361a05aa4b7c27c02a9e4
rorylkd <rory.donaldson14@gmail.com>
2021-05-11 10:44:02 +0800

    Added engineer and intern functions

--- index.js
+++ index.js
@@ -29,73 +29,112 @@ const initialQuestions = [
     name: "addTeamMember",
     message: "What would you like to do?",
     choices: ["Add an engineer", "Add an intern", "Finish building team"],
-
-},
+  },
 ];
 
 const engineerQuestions = [
-    {
-        type: "input",
-        name: "engineerName",
-        message: "What is your engineers name?",
-      },
-      {
-        type: "input",
-        name: "engineerID",
-        message: "What is your engineers ID?",
-      },
-      {
-        type: "input",
-        name: "engineerEmail",
-        message: "What is your engineers email adress?",
-      }, 
-      {
-        type: "input",
-        name: "engineerGithub",
-        message: "What is your engineers Github username?",
-      },   
+  {
+    type: "input",
+    name: "engineerName",
+    message: "What is your engineers name?",
+  },
+  {
+    type: "input",
+    name: "engineerID",
+    message: "What is your engineers ID?",
+  },
+  {
+    type: "input",
+    name: "engineerEmail",
+    message: "What is your engineers email adress?",
+  },
+  {
+    type: "input",
+    name: "engineerGithub",
+    message: "What is your engineers Github username?",
+  },
+  {
+    type: "list",
+    name: "addTeamMember",
+    message: "What would you like to do?",
+    choices: ["Add an engineer", "Add an intern", "Finish building team"],
+  },
 ];
 
 const internQuestions = [
-    {
-        type: "input",
-        name: "internName",
-        message: "What is your interns name?",
-      }, 
-      {
-        type: "input",
-        name: "internID",
-        message: "What is your interns ID?",
-      },   
-      {
-        type: "input",
-        name: "internEmail",
-        message: "What is your interns email address?",
-      },   
-      {
-        type: "input",
-        name: "internSchool",
-        message: "What is your interns school?",
-      },     
+  {
+    type: "input",
+    name: "internName",
+    message: "What is your interns name?",
+  },
+  {
+    type: "input",
+    name: "internID",
+    message: "What is your interns ID?",
+  },
+  {
+    type: "input",
+    name: "internEmail",
+    message: "What is your interns email address?",
+  },
+  {
+    type: "input",
+    name: "internSchool",
+    message: "What is your interns school?",
+  },
+  {
+    type: "list",
+    name: "addTeamMember",
+    message: "What would you like to do?",
+    choices: ["Add an engineer", "Add an intern", "Finish building team"],
+  },
 ];
 
-function start(){
-    inquirer.prompt(initialQuestions).then((initialAnswers) => {
-        console.log(initialAnswers);
+function start() {
+  inquirer.prompt(initialQuestions).then((initialAnswers) => {
+    console.log("initialAnswers:", initialAnswers);
 
-        var choice = initialAnswers.addTeamMember;
-        if (choice === 'Add an engineer') {
-    
-        } else if (choice === 'Add an intern') {
-
-        }else if (choice === 'Finish building team'){}
-        
-        });
-
+    var choice = initialAnswers.addTeamMember;
+    if (choice === "Add an engineer") {
+      engineer();
+    } else if (choice === "Add an intern") {
+      intern();
+    } else if (choice === "Finish building team") {
+      exit();
+    }
+  });
 }
 
+function engineer() {
+  inquirer.prompt(engineerQuestions).then((engineerAnswers) => {
+    console.log("engineerAnswers:", engineerAnswers);
+
+    var choice = engineerAnswers.addTeamMember;
+    if (choice === "Add an engineer") {
+      engineer();
+    } else if (choice === "Add an intern") {
+      intern();
+    } else if (choice === "Finish building team") {
+      exit();
+    }
+  });
+}
+
+function intern() {
+  inquirer.prompt(internQuestions).then((internAnswers) => {
+    console.log("internAnswers:", internAnswers);
+
+    var choice = internAnswers.addTeamMember;
+    if (choice === "Add an engineer") {
+      engineer();
+    } else if (choice === "Add an intern") {
+      intern();
+    } else if (choice === "Finish building team") {
+      exit();
+    }
+  });
+}
+
+function exit() {}
 
 start();
-
-
-
