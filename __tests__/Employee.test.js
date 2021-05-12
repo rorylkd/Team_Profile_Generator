557439c714780c301c8e9904c9606a49b3f90307
rorylkd <rory.donaldson14@gmail.com>
2021-05-12 09:48:48 +0800

    Started building real tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,10 @@
-
-const { test, expect } = require("@jest/globals");
+const { describe, test, expect } = require("@jest/globals");
 const Employee = require("../dist/js/Employee");
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(Employee(1,2)).toBe(3);
+describe("Employee", () => {
+  test("throws an error if inputs are empty", () => {
+    expect(() => {
+        new Employee();
+    }).toThrow("Please provide answers to all the prompts");
+  });
 });
--- dist/js/Employee.js
+++ dist/js/Employee.js
@@ -1,5 +1,10 @@
-function Employee(a, b) {
-    return a + b;
+class Employee {
+    constructor(name, id, email){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
 }
 
 
