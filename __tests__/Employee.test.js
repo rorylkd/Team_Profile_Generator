22b4fb619f070d697e58c6dfaf1df7c737c66e4e
rorylkd <rory.donaldson14@gmail.com>
2021-05-13 12:29:29 +0800

    Added more tests to check validity of responses

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -17,4 +17,19 @@ describe("Employee", () => {
       new Employee("Amy", "", "hotmale@hotmail.com");
     }).toThrow("Please provide an id");
   });
+  test("throws an error if name includes numbers or special characters", () => {
+    expect(() => {
+      new Employee("X Æ A-Xii #1", 3, "hotmale@hotmail.com");
+    }).toThrow("Please provide a valid name");
+  });
+  test("throws an error if id includes letters or special characters", () => {
+    expect(() => {
+      new Employee("Amy", "Amy?", "hotmale@hotmail.com");
+    }).toThrow("Please provide a valid id");
+  });
+  test("throws an error if email does not include @", () => {
+    expect(() => {
+      new Employee("Amy", 3, "notAnEmail");
+    }).toThrow("Please provide a valid email");
+  });
 });
