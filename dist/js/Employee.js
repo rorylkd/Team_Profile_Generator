450c5b26a7dd130057636f5c5d4cc3aa33e47ba1
rorylkd <rory.donaldson14@gmail.com>
2021-05-13 14:06:58 +0800

    All validity check tetss passing

--- dist/js/Employee.js
+++ dist/js/Employee.js
@@ -1,7 +1,7 @@
 class Employee {
-  constructor(name, id, email) {
+  constructor(names, id, email) {
     //Checking validity of responses
-    const nameless = name === "";
+    const nameless = names === "";
     const emailless = email === "";
     const idless = id === "";
 
@@ -17,13 +17,14 @@ class Employee {
     if (!email.includes("@")) {
       throw Error("Please provide a valid email");
     }
-    // const regexLettersAndSpecialChars = /^[a-zA-Z!@#\$%\^\&*\)\(+=._-]/;
-    // if (id.match(regexLettersAndSpecialChars)) {
-    //   throw Error("Please provide a valid id");
-    // }
+
     const regexNumbersAndSpecialChars = /^[0-9!@#\$%\^\&*\)\(+=._-]/;
-    if (name.match(regexNumbersAndSpecialChars)) {
+    if (names.match(regexNumbersAndSpecialChars)) {
       throw Error("Please provide a valid name");
+    }
+    const regexLettersAndSpecialChars = /^[a-zA-Z!@#\$%\^\&*\)\(+=._-]/;
+    if (id.match(regexLettersAndSpecialChars)) {
+      throw Error("Please provide a valid id");
     }
   }
 }
