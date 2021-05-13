68ed95f5ac8850e021417991c677e4b74fc6df95
rorylkd <rory.donaldson14@gmail.com>
2021-05-13 12:46:01 +0800

    Passed valid email test

--- dist/js/Employee.js
+++ dist/js/Employee.js
@@ -1,30 +1,21 @@
 class Employee {
-    constructor(name, id, email){
-const nameless =
-name === ""; 
-const emailless = 
-email === "";
-const idless = 
-id === "";
+  constructor(name, id, email) {
+    //Checking validity of responses
+    const nameless = name === "";
+    const emailless = email === "";
+    const idless = id === "";
 
-if (nameless){
-    throw Error("Please provide a name");
-}
-if (emailless){
-    throw Error("Please provide an email")
-}
-if (idless){
-    throw Error("Please provide an id")
-}
-
-
-
+    if (nameless) {
+      throw Error("Please provide a name");
     }
-
-
-    
-
+    if (emailless) {
+      throw Error("Please provide an email");
+    }
+    if (idless) {
+      throw Error("Please provide an id");
+    }
+    if (!(email.includes('@'))){throw Error("Please provide a valid email");}
+  }
 }
-
 
 module.exports = Employee;
