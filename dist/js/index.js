e7664462c224db3ef3179aa799c5065b1893af53
rorylkd <rory.donaldson14@gmail.com>
2021-05-13 22:50:36 +0800

    Added comments

--- dist/js/index.js
+++ dist/js/index.js
@@ -5,8 +5,10 @@ const Manager = require("./Manager.js");
 const Intern = require("./Intern.js");
 const Engineer = require("./Engineer.js");
 
+//This empty array will be populated with the team member objects depending on user input
 const teamMembersArray = [];
 
+// We have several arrays of question objects below that are called depending on which inquirer prompt is called
 const managerQuestions = [
   {
     type: "input",
@@ -104,11 +106,16 @@ const whichEmployee = [
     choices: ["Add an intern", "Add an engineer"],
   },
 ];
-
+// There can only be one manager! Which is why the start function creates a new manager object from the Manager class,
+// then pushes that manager object onto the teamMembersArray. We go onto the menu function next, or the exit fucntion if the team is very small.
 function start() {
   inquirer.prompt(managerQuestions).then((managerAnswers) => {
-    // teamMembersArray.push(initialAnswers);
-    const newManagerObject = new Manager(managerAnswers.managerName, managerAnswers.managerID, managerAnswers.managerEmail, managerAnswers.managerOfficeNumber)
+    const newManagerObject = new Manager(
+      managerAnswers.managerName,
+      managerAnswers.managerID,
+      managerAnswers.managerEmail,
+      managerAnswers.managerOfficeNumber
+    );
     teamMembersArray.push(newManagerObject);
 
     var choice = managerAnswers.addTeamMember;
@@ -120,36 +127,7 @@ function start() {
   });
 }
 
-function engineer() {
-  inquirer.prompt(engineerQuestions).then((engineerAnswers) => {
-    // teamMembersArray.push(engineerAnswers);
-    const newEngineerObject = new Engineer(engineerAnswers.engineerName, engineerAnswers.engineerID, engineerAnswers.engineerEmail, engineerAnswers.engineerGithub);
-teamMembersArray.push(newEngineerObject);
-
-    var choice = engineerAnswers.addTeamMember;
-    if (choice === "Add another team member") {
-      addTeamMember();
-    } else {
-      exit();
-    }
-  });
-}
-
-function intern() {
-  inquirer.prompt(internQuestions).then((internAnswers) => {
-    // teamMembersArray.push(internAnswers);
-    const newInternObject = new Intern(internAnswers.internName, internAnswers.internID, internAnswers.internEmail, internAnswers.internSchool);
-teamMembersArray.push(newInternObject);
-
-    var choice = internAnswers.addTeamMember;
-    if (choice === "Add another team member") {
-      addTeamMember();
-    } else {
-      exit();
-    }
-  });
-}
-
+//This function directs us to the engineer function or the intern function depending on which choice the user picked.
 function addTeamMember() {
   inquirer.prompt(whichEmployee).then((whichEmployeeAnswers) => {
     var choice = whichEmployeeAnswers.addTeamMember;
@@ -161,6 +139,49 @@ function addTeamMember() {
   });
 }
 
+//Engineer function ceates a new engineer object from the engineer class and pushes it into the team array.
+// Then we're either taken back to the menu, or we exit.
+function engineer() {
+  inquirer.prompt(engineerQuestions).then((engineerAnswers) => {
+    const newEngineerObject = new Engineer(
+      engineerAnswers.engineerName,
+      engineerAnswers.engineerID,
+      engineerAnswers.engineerEmail,
+      engineerAnswers.engineerGithub
+    );
+    teamMembersArray.push(newEngineerObject);
+
+    var choice = engineerAnswers.addTeamMember;
+    if (choice === "Add another team member") {
+      addTeamMember();
+    } else {
+      exit();
+    }
+  });
+}
+
+// You get it
+function intern() {
+  inquirer.prompt(internQuestions).then((internAnswers) => {
+    // teamMembersArray.push(internAnswers);
+    const newInternObject = new Intern(
+      internAnswers.internName,
+      internAnswers.internID,
+      internAnswers.internEmail,
+      internAnswers.internSchool
+    );
+    teamMembersArray.push(newInternObject);
+
+    var choice = internAnswers.addTeamMember;
+    if (choice === "Add another team member") {
+      addTeamMember();
+    } else {
+      exit();
+    }
+  });
+}
+
+// This function prints a nice message and spits out the array into the console so I can see it
 function exit() {
   console.log("Your team profile has been generated!");
   console.log(teamMembersArray);
