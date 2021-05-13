0312a0c97b04bf108592be199c5158d251b091ed
rorylkd <rory.donaldson14@gmail.com>
2021-05-13 11:02:19 +0800

    Empty name test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,9 +2,9 @@ const { describe, test, expect } = require("@jest/globals");
 const Employee = require("../dist/js/Employee");
 
 describe("Employee", () => {
-  test("throws an error if inputs are empty", () => {
+  test("throws an error if no name is given", () => {
     expect(() => {
-        new Employee();
+        new Employee("", 3, "hotmale@hotmail.com");
     }).toThrow("Please provide answers to all the prompts");
   });
 });
--- dist/js/Employee.js
+++ dist/js/Employee.js
@@ -1,9 +1,18 @@
 class Employee {
     constructor(name, id, email){
-        this.id = id;
-        this.name = name;
-        this.email = email;
+const nameless =
+name === ""; 
+
+if (nameless){
+    throw Error("Please provide answers to all the prompts");
+}
+
+
+
     }
+
+
+    
 
 }
 
